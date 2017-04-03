e85ec6b297789d8aa2d15f133eeb3d7a16b6acd7
Govindarajan <502000726@medpro.com>
2017-04-03 12:48:56 -0400

    Added complete cartesian product method

--- CartesianProduct.java
+++ CartesianProduct.java
@@ -9,10 +9,9 @@ import java.util.List;
 //  The method CartesianPrdnoDups performs a Cartesian product ignoring same values and values with duplicate distances.
 public class CartesianProduct {
 
-	public static List<String> CartesianPrdnoDups(String filename) throws IOException {
+	public static List<String> ProductnoDups(String filename) throws IOException {
 		FileReader fr = new FileReader(filename);
 		BufferedReader br = new BufferedReader(fr);
-		int i = 0, l = 0;
 		String tmp;
 		List<String> lsfile = new ArrayList<String>();
 		List<String> crtPrd = new ArrayList<String>();
@@ -27,14 +26,44 @@ public class CartesianProduct {
 				crtPrd.add(lsfile.get(j) + " " + lsfile.get(k));
 			}
 		}
+		br.close();
+		return crtPrd;
+	}
+	
+	public static List<String> Product(String filename) throws IOException {
+		FileReader fr = new FileReader(filename);
+		BufferedReader br = new BufferedReader(fr);
+		String tmp;
+		List<String> lsfile = new ArrayList<String>();
+		List<String> crtPrd = new ArrayList<String>();
 
+		while ((tmp = br.readLine()) != null) {
+			lsfile.add(tmp);
+		}
+
+		for (int j = 0; j < lsfile.size(); j++) {
+			for (int k = 0; k < lsfile.size(); k++) {
+				// System.out.println(lsfile.get(j) + " "+ lsfile.get(k));
+				crtPrd.add(lsfile.get(j) + " " + lsfile.get(k));
+			}
+		}
+		
+		br.close();
 		return crtPrd;
 	}
 
 	// Implementation - for testing purpose
 	public static void main(String args[]) throws IOException {
 		List<String> list = new ArrayList<String>();
-		list = CartesianPrdnoDups(args[0]);
+		list = ProductnoDups(args[0]);
+		
+		for (String x : list) {
+			System.out.println(x);
+		}
+		
+		System.out.println("");
+		
+		list = Product(args[0]);
 
 		for (String x : list) {
 			System.out.println(x);
